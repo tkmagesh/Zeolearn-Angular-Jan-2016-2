030cbe0c7bc62b1983c4a42efd2152b924517a6a
tkmagesh <tkmagesh77@gmail.com>
2016-01-28 23:25:11 +0530

    Async - Callbacks and Events

---
+++ Day-08/01-AsyncProgramming/async.js
@@ -0,0 +1,59 @@
+/* Sync */
+function addSync(x,y){
+    console.log("[Provider] processing ", x, " and ", y);
+    var result = x + y;
+    console.log("[Provider] returning the result");
+    return result;
+}
+
+function addSyncClient(x,y){
+    console.log("[Consumer] triggering addSync");
+    var result = addSync(x,y);
+    console.log("[Consumer] result = ", result);
+}
+
+
+/* Async  - Callbacks*/
+function addAsync(x,y, onResult){
+    console.log("[Provider] processing ", x, " and ", y);
+    setTimeout(function(){
+        var result = x + y;
+        console.log("[Provider] returning the result");
+        if (typeof onResult === 'function')
+            onResult(result);
+    },5000);
+}
+
+function addAsyncClient(x,y){
+    console.log("[Consumer] triggering addSync");
+    addAsync(x,y, function(result){
+        console.log("[Consumer] result = ", result);
+    });
+}
+
+/* Async - Events */
+
+var addAsync = (function(){
+    var _subscriptions = [];
+    function doWork(x,y){
+        console.log("[Provider] processing ", x, " and ", y);
+        setTimeout(function(){
+            var result = x + y;
+            console.log("[Provider] returning the result");
+            _subscriptions.forEach(function(subscriptionFn){
+                subscriptionFn(result);
+            })
+        },5000);
+    }
+    function addSubscription(subscriptionFn){
+        _subscriptions.push(subscriptionFn);
+    }
+    return {
+        doWork : doWork,
+        addSubscription : addSubscription
+    }
+})();
+
+
+
+
